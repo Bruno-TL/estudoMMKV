efdbe17a03c60c210ca3f34472325483c6cac961
Thiago Bruno <tbgt35@hotmail.com>
2024-01-26 11:02:08 -0300

    Criando um app para estudo do mmkv

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TextInput, Button } from "react-native";
+import { styles } from "./styles";
+import { MMKV, useMMKVString, useMMKVObject, useMMKV } from "react-native-mmkv";
+import { useEffect, useState } from "react";
+
+// const storage = new MMKV({ id: "myapp" });
+
+type User = {
+  name: string;
+  email: string;
+};
 
 export default function App() {
+  const [name, setName] = useState("user.name");
+  const [email, setEmail] = useState("user.email");
+  const [user, setUser] = useMMKVObject<User>("user");
+
+  const storage = useMMKV({ id: "myapp" });
+
+  function handleSave() {
+    // storage.set("user", JSON.stringify({ name, email }));
+    setUser({ name, email });
+  }
+
+  function fetchUser() {
+    const data = storage.getString("user");
+    setUser(data ? JSON.parse(data) : {});
+  }
+
+  useEffect(() => {
+    const listener = storage.addOnValueChangedListener((changedKey) => {
+      fetchUser();
+    });
+
+    return () => listener.remove();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholder="Nome..."
+        style={styles.input}
+        onChangeText={setName}
+      />
+      <TextInput
+        placeholder="Email..."
+        style={styles.input}
+        onChangeText={setEmail}
+      />
+      <Button title="Salvar" onPress={handleSave} />
+
+      <Text style={styles.text}>
+        {user?.name} - {user?.email}
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
